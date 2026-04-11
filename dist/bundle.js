520d2589f6e71a1db2720a6cbe5fbd88fc15a0ba
IronBill25 <215311957+Ironbill25@users.noreply.github.com>
2026-04-11 16:55:24 -0600

    Build

--- dist/bundle.js
+++ dist/bundle.js
@@ -1681,15 +1681,25 @@
 
   // scratchjsblocks/unicode.js
   window.sjs_unicode = [
-    Block(BlockType.REPORTER, "textToUni", "Text [text] to Unicode", {
+    Block(BlockType.REPORTER, "textToHexUnicode", "Text [text] to Hex Unicode", {
       text: Argument("string", "Hello")
     }, ({ text }) => {
-      return text.split("").map((c) => "\\" + c.charCodeAt(0).toString(16)).join("");
+      return text.split("").map((c) => "U+" + c.charCodeAt(0).toString(16).toUpperCase().padStart(4, "0")).join(" ");
     }),
-    Block(BlockType.REPORTER, "uniToText", "Unicode [uni] to Text", {
-      uni: Argument("string", "\\48\\65\\6c\\6c\\6f")
+    Block(BlockType.REPORTER, "hexUnicodeToText", "Hex Unicode [hex] to Text", {
+      hex: Argument("string", "U+0048 U+0065 U+006C U+006C U+006F")
+    }, ({ hex }) => {
+      return hex.split(" ").map((c) => String.fromCharCode(parseInt(c.replace("U+", ""), 16))).join("");
+    }),
+    Block(BlockType.REPORTER, "textToUnicode", "Text [text] to Unicode", {
+      text: Argument("string", "Hello")
+    }, ({ text }) => {
+      return text.split("").map((c) => c.charCodeAt(0)).join(" ");
+    }),
+    Block(BlockType.REPORTER, "unicodeToText", "Unicode [uni] to Text", {
+      uni: Argument("string", "72 101 108 108 111")
     }, ({ uni }) => {
-      return uni.split("\\").map((c) => String.fromCharCode(parseInt(c, 16))).join("");
+      return uni.split(" ").map((c) => String.fromCharCode(parseInt(c))).join("");
     })
   ];
 })();
