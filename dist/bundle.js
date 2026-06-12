bd67e689dc4188add99af698c04e78171ca7a36f
IronBill25 <215311957+Ironbill25@users.noreply.github.com>
2026-06-11 19:53:28 -0600

    try to avoid conflicts with existing variable blocks

--- dist/bundle.js
+++ dist/bundle.js
@@ -823,14 +823,14 @@
   // scratchjsblocks/variables.js
   window.sjs_variables = [
     Block(BlockType.BUTTON, "variablesCategory", "Variables"),
-    Block(BlockType.COMMAND, "createVariable", "Create [type] Variable [name]", {
+    Block(BlockType.COMMAND, "sjscreateVariable", "Create [type] Variable [name]", {
       type: ArgumentWithMenu("string", "global", "variableTypeMenu"),
       name: Argument("string", "myVariable")
     }, ({ type, name: name2 }, util) => {
       window.variableManager.createVariable(util, type, name2, "");
       vm.runtime.requestBlocksUpdate();
     }),
-    Block(BlockType.COMMAND, "createVariableWithValue", "Create [type] Variable [name] with value [value]", {
+    Block(BlockType.COMMAND, "sjscreateVariableWithValue", "Create [type] Variable [name] with value [value]", {
       type: ArgumentWithMenu("string", "global", "variableTypeMenu"),
       name: Argument("string", "myVariable"),
       value: Argument("string", "0")
@@ -838,33 +838,33 @@
       window.variableManager.createVariable(util, type, name2, value);
       vm.runtime.requestBlocksUpdate();
     }),
-    Block(BlockType.REPORTER, "getVariable", "Variable [name]", {
+    Block(BlockType.REPORTER, "sjsgetVariable", "Variable [name]", {
       name: ArgumentWithMenu("string", "myVariable", "variableMenu")
     }, ({ name: name2 }, util) => {
       const variable = window.variableManager.getVariable(util, name2);
       return variable.value;
     }),
-    Block(BlockType.COMMAND, "setVariable", "Set Variable [name] to [value]", {
+    Block(BlockType.COMMAND, "sjssetVariable", "Set Variable [name] to [value]", {
       name: ArgumentWithMenu("string", "myVariable", "variableMenu"),
       value: Argument("string", "0")
     }, ({ name: name2, value }, util) => {
       const variable = window.variableManager.getVariable(util, name2);
       variable.value = value;
     }),
-    Block(BlockType.COMMAND, "changeVariable", "Change Variable [name] by [value]", {
+    Block(BlockType.COMMAND, "sjschangeVariable", "Change Variable [name] by [value]", {
       name: ArgumentWithMenu("string", "myVariable", "variableMenu"),
       value: Argument("string", "1")
     }, ({ name: name2, value }, util) => {
       const variable = window.variableManager.getVariable(util, name2);
       variable.value = parseFloat(variable.value) + parseFloat(value);
     }),
-    Block(BlockType.BOOLEAN, "variableExists", "Variable [name] exists?", {
+    Block(BlockType.BOOLEAN, "sjsvariableExists", "Variable [name] exists?", {
       name: Argument("string", "myVariable")
     }, ({ name: name2 }, util) => {
       const variable = window.variableManager.getVariable(util, name2);
       return variable !== void 0;
     }),
-    Block(BlockType.COMMAND, "deleteVariable", "Delete Variable [name]", {
+    Block(BlockType.COMMAND, "sjsdeleteVariable", "Delete Variable [name]", {
       name: ArgumentWithMenu("string", "myVariable", "variableMenu")
     }, ({ name: name2 }, util) => {
       const variable = window.variableManager.getVariable(util, name2);
--- scratchjsblocks/variables.js
+++ scratchjsblocks/variables.js
@@ -1,13 +1,13 @@
 window.sjs_variables = [
     Block(BlockType.BUTTON, "variablesCategory", "Variables"),
-    Block(BlockType.COMMAND, "createVariable", "Create [type] Variable [name]", {
+    Block(BlockType.COMMAND, "sjscreateVariable", "Create [type] Variable [name]", {
         type: ArgumentWithMenu("string", "global", "variableTypeMenu"),
         name: Argument("string", "myVariable"),
     }, ({type, name}, util) => {
         window.variableManager.createVariable(util, type, name, '');
         vm.runtime.requestBlocksUpdate(); // only needed for when we make new variables
     }),
-    Block(BlockType.COMMAND, "createVariableWithValue", "Create [type] Variable [name] with value [value]", {
+    Block(BlockType.COMMAND, "sjscreateVariableWithValue", "Create [type] Variable [name] with value [value]", {
         type: ArgumentWithMenu("string", "global", "variableTypeMenu"),
         name: Argument("string", "myVariable"),
         value: Argument("string", "0"),
@@ -15,33 +15,33 @@ window.sjs_variables = [
         window.variableManager.createVariable(util, type, name, value);
         vm.runtime.requestBlocksUpdate();
     }),
-    Block(BlockType.REPORTER, "getVariable", "Variable [name]", {
+    Block(BlockType.REPORTER, "sjsgetVariable", "Variable [name]", {
         name: ArgumentWithMenu("string", "myVariable", "variableMenu"),
     }, ({name}, util) => {
         const variable = window.variableManager.getVariable(util, name);
         return variable.value;
     }),
-    Block(BlockType.COMMAND, "setVariable", "Set Variable [name] to [value]", {
+    Block(BlockType.COMMAND, "sjssetVariable", "Set Variable [name] to [value]", {
         name: ArgumentWithMenu("string", "myVariable", "variableMenu"),
         value: Argument("string", "0"),
     }, ({name, value}, util) => {
         const variable = window.variableManager.getVariable(util, name);
         variable.value = value;
     }),
-    Block(BlockType.COMMAND, "changeVariable", "Change Variable [name] by [value]", {
+    Block(BlockType.COMMAND, "sjschangeVariable", "Change Variable [name] by [value]", {
         name: ArgumentWithMenu("string", "myVariable", "variableMenu"),
         value: Argument("string", "1"),
     }, ({name, value}, util) => {
         const variable = window.variableManager.getVariable(util, name);
         variable.value = parseFloat(variable.value) + parseFloat(value);
     }),
-    Block(BlockType.BOOLEAN, "variableExists", "Variable [name] exists?", {
+    Block(BlockType.BOOLEAN, "sjsvariableExists", "Variable [name] exists?", {
         name: Argument("string", "myVariable"),
     }, ({name}, util) => {
         const variable = window.variableManager.getVariable(util, name);
         return variable !== undefined;
     }),
-    Block(BlockType.COMMAND, "deleteVariable", "Delete Variable [name]", {
+    Block(BlockType.COMMAND, "sjsdeleteVariable", "Delete Variable [name]", {
         name: ArgumentWithMenu("string", "myVariable", "variableMenu"),
     }, ({name}, util) => {
         const variable = window.variableManager.getVariable(util, name);
